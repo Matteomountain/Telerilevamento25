6da60ac39c4ac126ef31fa9aae55de9acdd4158c
Matteo Sorde <sordematt@gmail.com>
2025-04-10 15:34:45 +0200

    Update 08_spatial_variability.R

--- Code/08_spatial_variability.R
+++ Code/08_spatial_variability.R
@@ -32,6 +32,9 @@ stdev= sqrt(variance) #sqrt è la radice quadrata, stdev è la deviazione standa
 # im.list()
 
 sent = im.import("sentinel.png")
+sent = flip(sent)
+plot(sent)
+
 # Bande: 
 # 1 = nir
 # 2 = red
@@ -86,7 +89,83 @@ p1 + p2
 p3 = ggRGB(sent, r = 1, g = 2, b = 3 )
 p1 + p2 + p3
 
+# 
+
+ncell(sent)   # Pixel totali dell'immagine sent di ogni livello, dobbiamo moltiplicare per numero di layer -  nlyr
+
+ncell(sent) * nlyr(sent)
+# 794 * 798
+# 2534448
+
+senta = aggregate(sent, fact=2) # fact è l'aumento del fattore, se era 1x1 ora è 2x2 --> aumento il numero di pixel
+senta
+ncell(senta) * nlyr(senta)
+# 633612
+# 2534448 / 633612 = 4 
+
+senta5 = aggregate(sent, fact=5)
+senta5
+
+ncell(senta5) * nlyr(senta5)
+# 101760
+# 2534448 / 101760 = 24.9 
+
+# make a multiframe and plot in RGB three images (or, 2, 5)
+im.mulitframe(1,3)
+
+im.plotRGB(sent, 1, 2, 3)
+im.plotRGB(senta, 1, 2, 3)
+im.plotRGB(senta5, 1, 2, 3)
+
+# Standard deviation
+nira = senta [[1]]
+sd3a = focal(nira, w= c(3,3), fun= "sd")
+
+# Standard deviation for the factor 5 image
+nira5 = senta5 [[1]]
+sd3a5 = focal(nira5, w= c(3,3), fun= "sd")
+
+sd5a5 = focal(nira5, w=c(5,5), fun="sd")
+plot(sd5a5)
+
+im.multiframe(1,2)
+plot(sd5a5)
+plot(sd3a)
+
+im.multiframe(2,2)
+plot(sd3)
+plot(sd3a)
+plot(sd3a5)
+plot(sd5a5)
+
+p1 = im.ggplot(sd3)
+p2 = im.ggplot(sd3a)
+p3 = im.ggplot(sd3a5)
+p4 = im.ggplot(sd5a5)
+
+p1 + p2 + p3 + p4
 
 
+im.multiframe(2,2)
+plot(sd3, col=mako(100))
+plot(sd3a, col=mako(100))
+plot(sd3a5, col=mako(100))
+plot(sd5a5, col=mako(100))
+
+# Variance
+# nir
+
+var3 = sd3^2
+plot(var3) # Valorizza al massimo gli estremi, ma perdiamo tutta la parte di variabilità intermedia
+
+im.multiframe(1,2)
+plot(sd3)
+plot(var3)
+
+sd5 = focal(nir, w=c(5,5), fun= "sd")
+var5 = sd5^2
+
+plot(sd5)
+plot(var5)
 
 
