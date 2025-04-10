bf5ab843316cd4ebbfffaf616545517dde630cee
Matteo Sorde <sordematt@gmail.com>
2025-04-10 15:51:16 +0200

    Create 09_packages_from_gith.R

---
+++ Code/09_packages_from_gith.R
@@ -0,0 +1,31 @@
+# R code for installing packages from GitHub
+
+library(devtools) # or remotes
+
+# From GitHub
+install_github("ducciorocchini/cblindplot") # pacchetto per colori 
+library(cblindplot) 
+
+install_github("clauswilke/colorblindr")
+library(colorblindr)
+
+
+# From CRAN
+install.packages("colorblindcheck")
+library(colorblindcheck)
+
+# Se un autore muore, i pacchetti su CRAN che non vengono aggiornati vengono rimossi dal sito
+# Su GitHub invece rimane per sempre sul sito, si vede solo l'ultima attività sul profilo
+
+
+
+
+
+
+
+
+
+
+
+
+
